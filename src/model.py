2b2b31dba8ad61dbaef3d49aa8585bd7ac7228d5
Alex Rose <thornrose.alex@gmail.com>
2020-09-14 16:16:27 -0600

    Pushing for E2 instance

--- src/model.py
+++ src/model.py
@@ -89,9 +89,18 @@ def predict_and_store(model, user_ids, X_test, y_test, store_name="", verbose=Tr
         scores(y_test,y_pred)
     return y_prob
 
-def create_user_set(user_dict, demo_df, gam_df):
+def filter_low_activity(user_ids, activity=20):
+    filt_users = []
+    for user_id in user_ids:
+        mask = (gam_df['user_id'] == user_id)
+        user_daily = gam_df[mask]
+        if user_daily['weighted_bets'].sum() > activity:
+            filt_users.append(user_id)
+    return filt_users
+
+def create_user_set(user_ids, demo_df, gam_df):
     print("Entering set creation")
-    ids = pipeline.filter_low_activity(user_dict, gam_df=gam_df)
+    ids = filter_low_activity(user_ids)
     demo_filt = demo_df[demo_df.index.isin(ids)]
     rg_ids = list(demo_filt[demo_filt['rg'] == 1].index)
     no_rg_ids = list(demo_filt[demo_filt['rg'] == 0].index)
@@ -99,27 +108,22 @@ def create_user_set(user_dict, demo_df, gam_df):
    
 if __name__ == '__main__':
     user_ids = list(demo_df.index)
-    print("Using new stuff")
-    start = time.process_time()
-    gam_df = get_gam_df()
-    #rg_ids, no_rg_ids = create_user_set(user_dict, demo_df=demo_df, gam_df=gam_df)
-    #user_ids = rg_ids + random.choices(no_rg_ids, k=2000)
+    # print("Using new stuff")
+    # start = time.process_time()
+    # gam_df = get_gam_df()
+    # rg_ids, no_rg_ids = create_user_set(user_ids, demo_df=demo_df, gam_df=gam_df)
+    # user_ids = rg_ids + random.choices(no_rg_ids, k=len(rg_ids))
     #print(len(rg_ids))
     #print(len(no_rg_ids))
     #print(len(user_ids))
     # Random state to preserve same holdout (ideally I'd make MUCH more sure than this)
-    train_ids, holdout_ids = train_test_split(user_ids, random_state=104, shuffle=True)
+    train_ids = user_ids
     #features = ["total_hold"]
-    features = ["total_hold", "weekly_hold", "weekly_activity", "total_fixed_live_ratio"]
-    for look_forward in [1,3,6,9,12]:
+    #features = ["total_hold", "weekly_hold", "weekly_activity", "total_fixed_live_ratio"]
+    features = None
+    for look_forward in [6,12]:
         print(f"Beginning model with {look_forward} month look forward")
         X, y = featurize(train_ids, gam_df, features=features, look_forward=look_forward)
         X_train, X_test, y_train, y_test, user_train, user_test = train_test_split(X, y, train_ids)
-        regressor = train_random_forest(X_train, y_train, do_grid=False)
+        regressor = train_random_forest(X_train, y_train, do_grid=True)
         predict_and_store(regressor, user_test, X_test, y_test, store_name="validation")
-    ##
-    run_hold = False
-    if run_hold:
-        print("=========")
-        X_hold, y_hold = featurize(holdout_ids)
-        predict_and_store(regressor, X_hold, y_hold, store_name="holdout")
